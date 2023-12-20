2d22318ae83b22b2bf6a4c02a6015cb3e4ce51ca
surendra <surendra637877@gmail.com>
2023-12-20 16:52:02 +0530

    done

--- views/js/addtolike.js
+++ views/js/addtolike.js
@@ -1,8 +1,8 @@
-let productdiv = document.querySelector(".product-items-container");
+// let productdiv = document.querySelector(".product-items-container");
 let addcart = [];
-let cartbtn = document.getElementById("cartbtn");
+let likebtn = document.getElementById("like-btn");
 
-let categorylist = document.querySelector(".category");
+// let categorylist = document.querySelector(".category");
 let allCat = [];
 let displayproduct = async (allcheckcat = []) => {
   // categorylist.innerHTML = '';
@@ -134,27 +134,4 @@ function removeFromCart(index) {
 
 displayproduct();
 
-// let rawproduct = async () => {
-//   let product = await fetch("https://fakestoreapi.com/products");
-//   let finalproduct = await product.json();
-//   console.log(finalproduct);
-//   productdiv.innerHTML = "";
-//   finalproduct.forEach((element) => {
-//     productdiv.innerHTML += `
-//             <div class="product-div">
-//                 <img src=${element.image} alt="">
-//                 <span>${element.title}</span>
-//                 <div class="rating-price">
-//                     <div class="rating">
-//                         <i class="ri-star-fill"></i><i class="ri-star-fill"></i><i class="ri-star-fill"></i><i class="ri-star-fill"></i><i class="ri-star-fill"></i>
-//                     </div>
 
-//                     <div class="product-div-price">
-//                         ${element.price + " Rs"}
-//                     </div>
-//                 </div>
-//               </div>`;
-//   });
-// };
-
-// rawproduct();
